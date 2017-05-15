a8844d94d14936d6c78301f4360539b3a596237e
Joel Greenwood <greenwood.joel@gmail.com>
2017-05-15 08:33:24 -0400

    working on pid double out

--- PID-joystick-double-out/PID-joystick-double-out.ino
+++ PID-joystick-double-out/PID-joystick-double-out.ino
@@ -5,6 +5,8 @@ attempting to get joystick and direcional control (forwards and reverse) working
 
 #include <PID_v1.h>
 
+#define DEBUG 1
+
 #define joystick_pin A2 // joystick input 
 #define photores_forward_pin A3 // the photo senstive resistor input - pretending to be the string pot 
 #define photores_reverse_pin A4 // For this test I need a second sensor for the second LED.  This will change when both forward and reverse are acting on the the same sensor (string pot).
@@ -16,8 +18,12 @@ attempting to get joystick and direcional control (forwards and reverse) working
 #define N_AVG 8
 #define ADC_RES 12 //bits of resolution for ADC
 int ADC_num_of_bytes; // (pow(2, ADC_RES) - 1) --holds the (zero indexed) number of bytes for the ADC input 
-#define max_sensor_value 3600 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
-#define min_sensor_value 200 // This is the minimum read of the string pot - in this case the photo resistor. 
+#define max_sensor_value_forward 3910 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
+#define min_sensor_value_forward 2360 // This is the minimum read of the string pot - in this case the photo resistor. 
+#define max_sensor_value_reverse 3920 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
+#define min_sensor_value_reverse 2220 // This is the minimum read of the string pot - in this case the photo resistor. 
+int half_bytes;
+
 
 #define DAC_PWM_RES 12 // bits of resolution for PWM output
 int DAC_num_of_bytes; // (pow(2, DAC_PWM_RES) - 1)  --holds the (zero indexed) number of bytes for the PWM output
@@ -43,8 +49,10 @@ unsigned long lastMessage = 0;
 
 void setup()
 {
+  analogWriteResolution(DAC_PWM_RES);
   ADC_num_of_bytes = pow(2, ADC_RES) - 1;
   DAC_num_of_bytes = pow(2, DAC_PWM_RES) - 1;
+  half_bytes = ADC_num_of_bytes/2;
 
   pinMode(joystick_pin, INPUT);
   pinMode(photores_forward_pin, INPUT);
@@ -63,13 +71,18 @@ void setup()
 
   delay(500);
 
-  lightLevel_forward = adc->analogRead(photores_forward_pin);
-  Input = map(lightLevel_forward, min_sensor_value, max_sensor_value, 0, ADC_num_of_bytes);
-  lightLevel_reverse = adc->analogRead(photores_reverse_pin);
-  Input = map(lightLevel_reverse, min_sensor_value, max_sensor_value, 0, ADC_num_of_bytes);
-
   Setpoint = map(adc->analogRead(joystick_pin), 0, ADC_num_of_bytes, 0, ADC_num_of_bytes); // this seems like a silly mappng for now but the first max will change
 
+  if (Setpoint > half_bytes) {
+    lightLevel_forward = adc->analogRead(photores_forward_pin);
+    Input = map(lightLevel_forward, min_sensor_value_forward, max_sensor_value_forward, half_bytes, ADC_num_of_bytes);
+  }
+  else {
+    lightLevel_reverse = adc->analogRead(photores_reverse_pin);
+    Input = map(lightLevel_reverse, min_sensor_value_reverse, max_sensor_value_reverse, half_bytes, 0);
+  }
+
+  
 
   Serial.begin(9600);
   //myPID.SetOutputLimits() // This might be helpful depeding on what I do with scaling
@@ -84,15 +97,35 @@ void loop()
 {
   // The joystick is the setpoint. For now it will be forward or reverse velocity.  Later it could be position.
   // The joystick will be split (with a deadband) into forward and reverse velocity.
-  Setpoint = map(adc->analogRead(joystick_pin), 0, bits, 0, bits);
+  Setpoint = map(adc->analogRead(joystick_pin), 0, ADC_num_of_bytes, 0, ADC_num_of_bytes);
 
+  if (Setpoint > half_bytes) {
+    lightLevel_forward = adc->analogRead(photores_forward_pin);
+    Input = map(lightLevel_forward, min_sensor_value_forward, max_sensor_value_forward, half_bytes, ADC_num_of_bytes);
+  }
+  else {
+    lightLevel_reverse = adc->analogRead(photores_reverse_pin);
+    Input = map(lightLevel_reverse, min_sensor_value_reverse, max_sensor_value_reverse, half_bytes, 0);
+  }
 
-  lightLevel_forward = adc->analogRead(photores_pin);
-  //Serial.print("LIGHTLEVEL - raw: ");
-  //Serial.println(lightLevel_forward);
-  Input = map(lightLevel_forward, min_sensor_value, max_sensor_value, 0, bits);
   myPID.Compute();
-  analogWrite(forward_LED_pin, Output);
+
+  #ifdef DEBUG
+    Serial.print("Setpoint: ");
+    Serial.println(Setpoint);
+    Serial.print("Input: ");
+    Serial.print(Input);
+  #endif
+
+  
+
+  if (Setpoint > half_bytes) {
+    analogWrite(forward_LED_pin, Output);
+  }
+  else {
+    analogWrite(reverse_LED_pin, Output);
+  }
+
   now = millis();
   if(now - lastMessage > serialPing) {
         Serial.print("Setpoint = ");
