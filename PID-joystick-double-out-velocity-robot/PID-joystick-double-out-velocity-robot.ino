7457eb000175ebb1a8f42963d2d5d72b2f4263b4
joelgreenwood <greenwood.joel@gmail.com>
2017-05-18 09:28:11 -0400

    PID velocity and position control working on robot

--- PID-joystick-double-out-velocity-robot/PID-joystick-double-out-velocity-robot.ino
+++ PID-joystick-double-out-velocity-robot/PID-joystick-double-out-velocity-robot.ino
@@ -5,16 +5,35 @@ attempting to get joystick and direcional control (forwards and reverse) working
 
 #include <PID_v1.h>
 #include <Bounce2.h>
-
+#include "RunningAverage.h"
 //#define DEBUG 1
 
-#define deadman_pin 25 // 5 on test, 25 on bot - Stokes board
+int sensorAverageNumber = 10;
+int velocityAverageNumber = 10;
+RunningAverage hipSensorAverage(sensorAverageNumber);
+RunningAverage hipVelocityAverage(velocityAverageNumber);
+#define loopDelay 10000 // microseconds beween velocity calculations
+
+float lastSensorReading = 0;
+float lastVelocity = 0;
+float currentVelocity = 0;
+float targetVelocity = 0;
+elapsedMicros sinceReading;
+unsigned long last_time = 0;
+unsigned long now = 0;
+
+#define hip_min_velocity 0
+#define hip_max_velocity 4095 // sensor units per second 
+unsigned long hip_max_velocity_microS;
+float tempVelocity;
+
+#define deadman_pin 25 // 5 on test board - 25 on new board
 Bounce deadmanBounce = Bounce(deadman_pin, 20);
 
 #define enable_pin 2 // for the motor driver board
 
-#define joystick_pin 31 // A2 for test - 31 for bot --joystick input output
-#define hip_pot_pin 20 // A3 for test, 20 for bot --the photo senstive resistor input - pretending to be the string pot 
+#define joystick_pin 31 // A2 on test - 31 --joystick input on bot
+#define hip_pot_pin 20 // A3 on test - 20  on bot--the photo senstive resistor input - pretending to be the string pot 
 //#define photores_reverse_pin A4 // For this test I need a second sensor for the second LED.  This will change when both forward and reverse are acting on the the same sensor (string pot).
 #define forward_hip_pin 9 // output pin for forward direction - in this case an LED but next it will be the valve
 #define reverse_hip_pin 10 // output pin for reverse 
@@ -24,20 +43,20 @@ Bounce deadmanBounce = Bounce(deadman_pin, 20);
 #define N_AVG 8
 #define ADC_RES 12 //bits of resolution for ADC
 int ADC_num_of_bytes; // (pow(2, ADC_RES) - 1) --holds the (zero indexed) number of bytes for the ADC input 
-#define max_hip_sensor_value 2896 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
-#define min_hip_sensor_value 376 // This is the minimum read of the string pot - in this case the photo resistor. 
+#define max_hip_sensor_value ADC_num_of_bytes // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
+#define min_hip_sensor_value 0 // This is the minimum read of the string pot - in this case the photo resistor. 
 
 
 //#define max_sensor_value_reverse 3800 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
 //#define min_sensor_value_reverse 500// This is the minimum read of the string pot - in this case the photo resistor. 
-int half_bytes;
-#define setpoint_deadband 0 //This is the difference accaptable between the setpoint (joystick) position and the input (sensor readout).
+
+#define setpoint_deadband 50 //This is the difference accaptable between the setpoint (joystick) position and the input (sensor readout).
 //#define PWM_deadband 0 //number of bytes required before there is a positive response from the valve.  This will be relative to the bit rate.
-#define PWM_deadband_percent 30 //percent of PWM range needed to crack the valve
+#define PWM_deadband_percent 20 //percent of PWM range needed to crack the valve
 long PWM_deadband = 0;
-#define pwm_govenor 100 //percent of maximum output
 int pwm_output = 0;
 unsigned long max_PWM_output; // This caps the PWM output so we don't blow things up. Today... 
+#define pwm_govenor 100 //percent of maximum output
 
 #define DAC_PWM_RES 12 // bits of resolution for PWM output
 int DAC_num_of_bytes; // (pow(2, DAC_PWM_RES) - 1)  --holds the (zero indexed) number of bytes for the PWM output
@@ -57,16 +76,18 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 const int sampleRate = 1;
 const int serialPing = 100; //interval in ms at which serial is sent and read
-unsigned long now = 0;
+unsigned long now_ms = 0;
 unsigned long lastMessage = 0;
 
 void setup()
 {
+  //hip_max_velocity_microS = hip_max_velocity * 1000000;
+  hipVelocityAverage.clear();
+
   analogWriteResolution(DAC_PWM_RES);
   analogWriteFrequency(9, 11718);
   ADC_num_of_bytes = pow(2, ADC_RES) - 1;
   DAC_num_of_bytes = pow(2, DAC_PWM_RES) - 1;
-  half_bytes = ADC_num_of_bytes/2;
 
   max_PWM_output = ADC_num_of_bytes * ((float)pwm_govenor/100);
   PWM_deadband = ADC_num_of_bytes * ((float)PWM_deadband_percent/100);
@@ -100,21 +121,40 @@ void setup()
 
 void loop()
 {
+  delay(1);
   deadmanBounce.update(); //update the deadman
-  
-  // if (deadmanBounce.fell()) {
-  //   Serial.println("deadman fell");
-  // }
 
-  Setpoint = adc->analogRead(joystick_pin);
+  hipSensorAverage.addValue(adc->analogRead(hip_pot_pin));
 
-  hip_pot_value = adc->analogRead(hip_pot_pin);
-
-
-
-  Input = map(hip_pot_value, min_hip_sensor_value, max_hip_sensor_value, 0, ADC_num_of_bytes);  
-
-  myPID.Compute();
+  if (sinceReading > loopDelay) {
+    last_time = now;
+    now = micros();
+    lastSensorReading = hip_pot_value;
+    hip_pot_value = hipSensorAverage.getAverage();
+    int pot_change = hip_pot_value - lastSensorReading;
+    // Serial.print("pot_change:");
+    // Serial.print("\t");
+    // Serial.println(pot_change);
+    //Serial.print("\t");
+    // float tempVelocity = (hip_pot_value - lastSensorReading) / (now - last_time);
+    // Serial.print("\t");
+    // Serial.print("tmpVel:");
+    // Serial.print("\t");
+    // Serial.println(tempVelocity * 1000000);
+    if (pot_change == 0) {
+      tempVelocity = 0;
+    }
+    else {
+      tempVelocity = (hip_pot_value - lastSensorReading) / (now - last_time);
+    }
+    
+    hipVelocityAverage.addValue(tempVelocity);
+    currentVelocity = hipVelocityAverage.getAverage() * 1000000;
+    Setpoint = map(adc->analogRead(joystick_pin), 0, ADC_num_of_bytes, -hip_max_velocity, hip_max_velocity);
+    Input = map(currentVelocity, -hip_max_velocity, hip_max_velocity, -ADC_num_of_bytes, ADC_num_of_bytes);
+    myPID.Compute();
+    sinceReading = 0;
+  }
 
   if (deadmanBounce.read() == 1) { 
     digitalWrite(enable_pin, HIGH);
@@ -131,7 +171,6 @@ void loop()
     else {
       analogWrite(forward_hip_pin, 0);
       analogWrite(reverse_hip_pin, 0);
-      pwm_output = 0;
     }
   }
   else {
@@ -140,23 +179,30 @@ void loop()
     analogWrite(reverse_hip_pin, 0);
   }
 
-  now = millis();
-  if(now - lastMessage > serialPing) {
-        //Serial.print("hipPot: ");
-        //Serial.print((adc->analogRead(hip_pot_pin)));
-        Serial.print("Setpoint:");
+  now_ms = millis();
+  if(now_ms - lastMessage > serialPing) {
+        Serial.print("Velocity:");
         Serial.print("\t");
+        Serial.print(hipVelocityAverage.getAverage() * 1000000);
+        Serial.print("\t");
+        Serial.print("sensor:");
+        Serial.print("\t");
+        Serial.print(adc->analogRead(hip_pot_pin));
+        Serial.print("\t");
+        // Serial.print("millis:");
+        // Serial.print("\t");
+        // Serial.print(micros()/1000);
+        // Serial.print("\t");
+        Serial.print("Setpoint: ");
         Serial.print(Setpoint);
         Serial.print("\t");
-        Serial.print("Input:");
-        Serial.print("\t");
+        Serial.print("Input: ");
         Serial.print(Input);
         Serial.print("\t");
-        Serial.print("Output:");
-        Serial.print("\t");
+        Serial.print("Output: ");
         Serial.print(Output);
         Serial.print("\t");
-        Serial.print("PWM:");
+        Serial.print("PWM: ");
         Serial.print("\t");
         Serial.print(pwm_output);
         Serial.print("\n");
@@ -187,7 +233,7 @@ void loop()
                 Serial.println(Kd);
                 myPID.SetTunings(Kp, Ki, Kd);        
         }
-        lastMessage = now;
+        lastMessage = now_ms;
   }
 }
 
--- PID-joystick-double-out/PID-joystick-double-out.ino
+++ PID-joystick-double-out/PID-joystick-double-out.ino
@@ -4,8 +4,13 @@ attempting to get joystick and direcional control (forwards and reverse) working
 */
 
 #include <PID_v1.h>
+#include <Bounce2.h>
 
 //#define DEBUG 1
+
+#define deadman_pin 25
+Bounce deadmanBounce = Bounce(deadman_pin, 20);
+
 
 #define joystick_pin A2 // joystick input 
 #define photores_forward_pin A3 // the photo senstive resistor input - pretending to be the string pot 
@@ -18,12 +23,14 @@ attempting to get joystick and direcional control (forwards and reverse) working
 #define N_AVG 8
 #define ADC_RES 12 //bits of resolution for ADC
 int ADC_num_of_bytes; // (pow(2, ADC_RES) - 1) --holds the (zero indexed) number of bytes for the ADC input 
-#define max_sensor_value_forward 3910 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
-#define min_sensor_value_forward 2585 // This is the minimum read of the string pot - in this case the photo resistor. 
-#define max_sensor_value_reverse 3920 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
-#define min_sensor_value_reverse 2220 // This is the minimum read of the string pot - in this case the photo resistor. 
+#define max_sensor_value_forward 3800 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
+#define min_sensor_value_forward 500 // This is the minimum read of the string pot - in this case the photo resistor. 
+#define max_sensor_value_reverse 3800 // This is the maximum read of the string pot - in this case the photo resistor.  I will use the same numbers for forward and reverse to make it easier to convert to the string pot later.  
+#define min_sensor_value_reverse 500// This is the minimum read of the string pot - in this case the photo resistor. 
 int half_bytes;
-
+#define setpoint_deadband 1 //This is the difference accaptable between the setpoint (joystick) position and the input (sensor readout).
+#define PWM_deadband 0 //number of bytes required before there is a positive response from the valve.  This will be relative to the bit rate.
+//#define max_PWM_output 4000
 
 #define DAC_PWM_RES 12 // bits of resolution for PWM output
 int DAC_num_of_bytes; // (pow(2, DAC_PWM_RES) - 1)  --holds the (zero indexed) number of bytes for the PWM output
@@ -39,7 +46,7 @@ ADC *adc = new ADC(); // adc object
 double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
-double Kp=0, Ki=10, Kd=0;
+double Kp=0.01, Ki=10, Kd=0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 const int sampleRate = 1;
@@ -97,17 +104,22 @@ void setup()
 
 void loop()
 {
+  deadmanBounce.update(); //update the deadman
+  
   // The joystick is the setpoint. For now it will be forward or reverse velocity.  Later it could be position.
   // The joystick will be split (with a deadband) into forward and reverse velocity.
   Setpoint = map(adc->analogRead(joystick_pin), 0, ADC_num_of_bytes, -ADC_num_of_bytes, ADC_num_of_bytes);
 
-  if (Setpoint >= 0) {
+  if (Setpoint > setpoint_deadband) {
     lightLevel_forward = adc->analogRead(photores_forward_pin);
-    Input = map(lightLevel_forward, min_sensor_value_forward, max_sensor_value_forward, 0, ADC_num_of_bytes);
+    Input = (map(lightLevel_forward, min_sensor_value_forward, max_sensor_value_forward, 0, ADC_num_of_bytes)) + PWM_deadband;
+  }
+  else if (Setpoint < -setpoint_deadband) {
+    lightLevel_reverse = adc->analogRead(photores_reverse_pin);
+    Input = (map(lightLevel_reverse, min_sensor_value_reverse, max_sensor_value_reverse, 0, -ADC_num_of_bytes)) - PWM_deadband;
   }
   else {
-    lightLevel_reverse = adc->analogRead(photores_reverse_pin);
-    Input = map(lightLevel_reverse, min_sensor_value_reverse, max_sensor_value_reverse, 0, -ADC_num_of_bytes);
+    Input = 0;
   }
 
   myPID.Compute();
@@ -121,25 +133,30 @@ void loop()
 
   
 
-  if (Setpoint >= 0) {
+
+  if (Setpoint > setpoint_deadband) {
     analogWrite(forward_LED_pin, Output);
     analogWrite(reverse_LED_pin, 0);
   }
-  else {
+  else if (Setpoint < -setpoint_deadband) {
     analogWrite(reverse_LED_pin, -Output);
     analogWrite(forward_LED_pin, 0);
+  }
+  else {
+    analogWrite(forward_LED_pin, 0);
+    analogWrite(reverse_LED_pin, 0);
   }
 
   now = millis();
   if(now - lastMessage > serialPing) {
-        //Serial.print("raw forward sensor: ");
-        //Serial.print((adc->analogRead(photores_forward_pin)));
+        //Serial.print("joystick: ");
+        //Serial.print((adc->analogRead(joystick_pin)));
         Serial.print("Setpoint: ");
         Serial.print(Setpoint);
         Serial.print(" Input: ");
         Serial.print(Input);
         Serial.print(" Output: ");
-        Serial.print(Output);
+        Serial.print(Output * 2);
         //Serial.print(" LIGHTLEVEL - raw: ");
         //Serial.print(lightLevel_forward);
         Serial.print("\n");
